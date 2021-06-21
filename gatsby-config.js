d216e00ace6053b8e14f1e2d33903034bdd02f9a
Bryan C Guner <bryan.guner@gmail.com>
2021-06-21 15:09:35 +0000

    fixed maybe

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,6 @@ module.exports = {
         locale: "en",
         title: "Bryan Guner",
     },
-    module.exports = {
-  siteMetadata: {
-    description: "Personal page of Bryan Guner",
-    locale: "en",
-    title: "Bryan Guner",
-    formspreeEndpoint: "https://formspree.io/f/mdoyzvay",
-  },
     plugins: [
         {
             resolve: "@wkocjan/gatsby-theme-intro",
