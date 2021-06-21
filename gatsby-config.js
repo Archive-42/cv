c78a449717fea43f77ce6d4744cd91240225646a
Stackbit Code Editor <projects@stackbit.com>
2021-06-21 13:14:58 +0000

    gatsby-config.js: changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@ module.exports = {
         locale: "en",
         title: "Bryan Guner",
     },
+    module.exports = {
+  siteMetadata: {
+    description: "Personal page of John Doe",
+    locale: "en",
+    title: "John Doe",
+    formspreeEndpoint: "https://formspree.io/f/{your-id}",
+  },
     plugins: [
         {
             resolve: "@wkocjan/gatsby-theme-intro",
